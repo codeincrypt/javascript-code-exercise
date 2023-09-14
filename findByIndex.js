69985b7c9f22ab270ca5d389f4f5b38a01990a4c
codeincrypt <kartikkr555@gmail.com>
2023-09-15 03:22:42 +0530

    find index of any key pair value

---
+++ findByIndex.js
@@ -0,0 +1,34 @@
+const arr = [
+  {
+    id: "1",
+    name: "BMW",
+  },
+  {
+    id: "2",
+    name: "Audi",
+  },
+  {
+    id: "3",
+    name: "Volvo",
+  },
+];
+/**
+ * Finds the index of the first occurrence of an object with name "BMW" in the given array.
+ * @param {Array} arr - The array to search.
+ * @returns {number} - The index of the object with name "BMW", or -1 if not found.
+ */
+const findByIndex = (arr) => {
+  const index = arr.map((i) => i.name).indexOf("BMW");
+  return index
+}
+console.log(findByIndex(arr)); // 👉️ 0
+
+const findByIndex2 = (arr) => {
+  // Map the array to an array of names
+  const names = arr.map((i) => i.name);
+  // Find the index of the first occurrence of "BMW"
+  const index = names.indexOf("Volvo");
+  // Return the index, or -1 if not found
+  return index;
+}
+console.log(findByIndex2(arr)); // 👉️ 2
